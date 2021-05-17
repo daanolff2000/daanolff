2ed5fc78632a85d1399bbc896c257a9772492a2c
Daan Olff <daanolff2000@gmail.com>
2021-05-17 22:43:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var body = document.querySelector('body');
+var toggle = document.querySelector('#toggle');
+
+
+toggle.addEventListener('click', function toggleMenu(){
+    body.classList.toggle('active');
+});
