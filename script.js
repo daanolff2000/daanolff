130cd99bcb7d8cae2b4461ec353b1ecfafb13fcc
Daan Olff <daanolff2000@gmail.com>
2021-05-18 22:54:19 +0200

    scroll to top

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ var toggle = document.querySelector('#toggle');
 toggle.addEventListener('click', function toggleMenu(){
     body.classList.toggle('active');
 });
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
