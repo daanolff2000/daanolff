be59ef0b98137c8e541246c373856d55d4c0747e
Daan Olff <daanolff2000@gmail.com>
2021-05-19 15:06:59 +0200

    blue-ish

--- script.js
+++ script.js
@@ -5,7 +5,3 @@ var toggle = document.querySelector('#toggle');
 toggle.addEventListener('click', function toggleMenu(){
     body.classList.toggle('active');
 });
-
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-}
